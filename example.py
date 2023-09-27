d6ef0b1d1fc4b4f6b8d2b05cff74e91a9986fa59
juliogazoli <julio_gazoli@hotmail.com>
2023-09-27 12:59:04 -0300

    Forçando Problemas

---
+++ example.py
@@ -0,0 +1,28 @@
+# print("Olá mundo")
+
+# print("Olá mundo"))
+
+# print "Olá mundo"
+
+# if True:
+# print("Olá mundo")
+
+# if True:
+#  print("Olá mundo")
+
+# frase = "Olá mundo"
+# print(frase.ola)
+
+# frase = "Olá mundo"
+# print(frase)
+
+def dividir(dividendo, divisor):
+    return dividendo / divisor
+
+
+def testa_divisao(divisor):
+    resultado = dividir(10, divisor)
+    print(f"O resultado da divisao de 10 por {divisor} é {resultado}")
+
+# testa_divisao(2)
+testa_divisao(0)
