c4f271b6ed1e04df96f1d697f15c3a94e3d52d2a
juliogazoli <julio_gazoli@hotmail.com>
2023-09-29 12:56:44 -0300

    Tratamento de Exceções

--- example.py
+++ example.py
@@ -1,41 +1,29 @@
-# print("Olá mundo")
-
-# print("Olá mundo"))
-
-# print "Olá mundo"
-
-# if True:
-# print("Olá mundo")
-
-# if True:
-#  print("Olá mundo")
-
-# frase = "Olá mundo"
-# print(frase.ola)
-
-# frase = "Olá mundo"
-# print(frase)
-
 def dividir(dividendo, divisor):
     # print(divisor.resultado)
     return dividendo / divisor
 
 
 def testa_divisao(divisor):
-    try:
-        # teste = 'teste'
-        # teste.ola()
-        resultado = dividir(10, divisor)
-        print(f"O resultado da divisao de 10 por {divisor} é {resultado}")
-    except ZeroDivisionError:
-        print("Erro de divisão por zero tratado")
-    # except AttributeError:
-    #     print("Erro de atributo tratado")
+    resultado = dividir(10, divisor)
+    print(f"O resultado da divisao de 10 por {divisor} é {resultado}")
+    
+# try:
+#     testa_divisao(0)
+# except AttributeError as E:
+#     print(E.__class__.__bases__)
+# except ZeroDivisionError as E:
+#         print(E.__class__.__bases__)
 
-# testa_divisao(2)
+# try:
+#     testa_divisao(0)
+# except Exception as E:
+#     print(E.__class__)
+
 try:
     testa_divisao(0)
-except AttributeError:
-    print("Erro de atributo tratado")
+except ZeroDivisionError as E:
+    print("Erro de divisão por zero")
+except Exception as E:
+    print("Tratamento genérico")
     
 print("Programa encerrado")
